7936ae9dfed4ad7ccfcf934c015a0b80c4b2614d
aheerdevta <yadavyugank321@gmail.com>
2026-01-01 05:01:12 +0000

    update

--- preptive/next.config.mjs
+++ preptive/next.config.mjs
@@ -1,55 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   async redirects() {
     return [];
   },
 
-  // ✅ Server Actions enable
-  experimental: {
-    serverActions: true,
-  },
-
-  // Add headers for sitemap.xml
   async headers() {
     return [
       {
         source: '/sitemap.xml',
         headers: [
-          {
-            key: 'Content-Type',
-            value: 'application/xml; charset=utf-8',
-          },
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=86400, stale-while-revalidate=43200',
-          },
+          { key: 'Content-Type', value: 'application/xml; charset=utf-8' },
+          { key: 'Cache-Control', value: 'public, max-age=86400, stale-while-revalidate=43200' },
         ],
       },
       {
         source: '/sitemap-categories.xml',
         headers: [
-          {
-            key: 'Content-Type',
-            value: 'application/xml; charset=utf-8',
-          },
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=86400, stale-while-revalidate=43200',
-          },
+          { key: 'Content-Type', value: 'application/xml; charset=utf-8' },
+          { key: 'Cache-Control', value: 'public, max-age=86400, stale-while-revalidate=43200' },
         ],
       },
       {
         source: '/sitemap-:id.xml',
         headers: [
-          {
-            key: 'Content-Type',
-            value: 'application/xml; charset=utf-8',
-          },
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=86400, stale-while-revalidate=43200',
-          },
+          { key: 'Content-Type', value: 'application/xml; charset=utf-8' },
+          { key: 'Cache-Control', value: 'public, max-age=86400, stale-while-revalidate=43200' },
         ],
       },
     ];
@@ -58,7 +33,18 @@ const nextConfig = {
   trailingSlash: false,
 
   images: {
-    domains: ['image.preptive.in', 'www.preptive.in'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.preptive.in',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.preptive.in',
+        pathname: '/**',
+      },
+    ],
     formats: ['image/avif', 'image/webp'],
   },
 };
